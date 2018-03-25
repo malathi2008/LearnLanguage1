b6cbae53492d905ff0e686d097814fc1a57569bb
Shruti <Shruti@DESKTOP-SVPK3QT>
2018-03-25 16:54:12 -0400

    updated final project

--- app/src/main/java/com/student/learnlanguage/connection/Const.java
+++ app/src/main/java/com/student/learnlanguage/connection/Const.java
@@ -1,6 +1,6 @@
 package com.student.learnlanguage.connection;
 
 public class Const {
-    public static final String SITE_URL = "http://192.168.2.202:8080/finalProject/final/generic/";
-    public static final String image_URL = "http://192.168.2.202:8080/finalProject/images/";
+    public static final String SITE_URL = "http://172.24.13.8:8080/finalProject/final/generic/";
+    public static final String image_URL = "http://172.24.13.8:8080/finalProject/images/";
 }
